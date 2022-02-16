063fff6da4414b166c4453a68f4e04261de50ad2
AndreFrigo <andrea.boy.frigo@gmail.com>
2022-02-16 11:50:37 +0100

    Add plot function

---
+++ plot.py
@@ -0,0 +1,19 @@
+# plot a set of points and lines
+def plot(points, lines = [], figname = "plot"):
+    import matplotlib.pyplot as plt
+    # plot points
+    px = [elem[0] for elem in points]
+    py = [elem[1] for elem in points]
+    plt.scatter(px, py, color = 'black')
+    # plot lines
+    for line in lines:
+        x = [line[0][0], line[1][0]]
+        y = [line[0][1], line[1][1]]
+        plt.plot(x, y, linewidth=2, color = 'black')
+    
+    plt.show()
+    plt.savefig("plots/"+figname+".png")
+
+
+# This script can be used also to plot points from a txt file
+#TODO
