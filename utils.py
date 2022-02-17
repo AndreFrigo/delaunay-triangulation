9d567120889782d34af5a40443e0018256b1288e
AndreFrigo <andrea.boy.frigo@gmail.com>
2022-02-17 19:41:30 +0100

    Add function to check if a point is inside the circle made from a triangle

--- utils.py
+++ utils.py
@@ -41,3 +41,23 @@ def readInput(inputFile="input.txt"):
         s = s.split(',')
         ret.append((int(s[0]), int(s[1])))
     return ret
+
+# Checks if a point 'p' is inside the circle made from the points of the triangle 'triangle'
+# returns true if p is inside the circle
+def isInsideCircle(p, triangle):
+    b,c,d=triangle
+    temp = c[0]**2 + c[1]**2
+    bc = (b[0]**2 + b[1]**2 - temp) / 2
+    cd = (temp - d[0]**2 - d[1]**2) / 2
+    det = (b[0] - c[0]) * (c[1] - d[1]) - (c[0] - d[0]) * (b[1] - c[1])
+
+    if abs(det) < 1.0e-10:
+        return None
+
+    # Center of circle
+    cx = (bc*(c[1] - d[1]) - cd*(b[1] - c[1])) / det
+    cy = ((b[0] - c[0]) * cd - (c[0] - d[0]) * bc) / det
+    # Radius
+    radius = ((cx - b[0])**2 + (cy - b[1])**2)**.5
+    # Check if p is inside
+    return (p[0] - cx)**2 + (p[1] - cy)**2 <= radius**2
